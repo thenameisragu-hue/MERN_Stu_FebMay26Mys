2e14bbcc71dcb33cd83a944b0d288478a38a5239
Raghavendra H S <thenameisragu@gmail.com>
2026-03-04 14:41:02 +0530

    While Loop

---
+++ W3/D3/P2.JS
@@ -0,0 +1,15 @@
+//while loop
+console.log("While Loop");
+
+let count=0;
+while(count<5){
+    console.log("count : ",count);
+    count++;
+}
+
+//do while loop
+let value=10;
+do{
+    console.log("value = ",value);
+    value++;
+}while(value<3);
