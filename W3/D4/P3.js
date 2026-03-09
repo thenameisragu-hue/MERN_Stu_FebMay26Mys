dfc59c8f21ca7fb5ecb264936a943c640bacd0e6
Raghavendra H S <thenameisragu@gmail.com>
2026-03-09 07:37:20 +0530

    Array Mapping

---
+++ W3/D4/P3.js
@@ -0,0 +1,14 @@
+// array map
+let prices = [10,20,30,40];
+let pricewithGST = prices.map(price => price + price*0.18);
+console.log("price without tax",prices);
+console.log("price with tax",pricewithGST);
+
+// Using map to extract files
+let users = [
+    {name:"Appu", age:48},
+    {name:"Hruthik", age:21}
+];
+// to take individual object
+let names = users.map(user => user.name);
+console.log(" ",names);
