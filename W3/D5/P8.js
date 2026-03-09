70cc6a7ee509860cfb8b7b1dc6483e77ad49bf38
Raghavendra H S <thenameisragu@gmail.com>
2026-03-09 07:47:48 +0530

    HTML:Named Handlers and removeEventListeners

---
+++ W3/D5/P8.js
@@ -0,0 +1,21 @@
+// Event delegation
+
+const tasklist = document.getElementById("taskList");
+const addTask = document.getElementById("addTask");
+let taskCount = 3;
+
+tasklist.addEventListener("click",function(event){
+    if(event.target.classList.contains("deleteBtn")){
+    console.log("Delete button clicked for:",
+        event.target.parentElement.textContent.trim()
+    );
+    event.target.parentElement.remove();
+    }
+});
+addTask.addEventListener("click",function(){
+    taskCount++;
+    const li=document.createElement("li");
+    li.innerHTML = "Task" + taskCount + '<button class="deleteBtn">Delete</button>';
+    tasklist.appendChild(li);
+    console.log("New task created");
+});
