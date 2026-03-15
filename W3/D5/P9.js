b88fbcc788bada4af6832c54d46c2799b1233d8a
Raghavendra H S <thenameisragu@gmail.com>
2026-03-15 21:06:37 +0530

    JS:Advanced Event Pattern and Global keyboard Shortcut

--- W3/D5/P9.js
+++ W3/D5/P9.js
@@ -2,7 +2,7 @@ const oncebtn=document.getElementById("oncebtn");
 
 //Limited listener to once only for a event listen
 oncebtn.addEventListener("click",function(){
-    console.log("This click listener works only once.");
+    console.log("This click listener works  only once.");
 } ,{once:true});
 
 //global keyboard shortcut creation
