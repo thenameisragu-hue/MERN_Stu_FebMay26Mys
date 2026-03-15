dc7a9ce650a19ef2541f560caeced60f299ca040
Raghavendra H S <thenameisragu@gmail.com>
2026-03-15 21:09:22 +0530

    JS:Programming Events

--- W3/D5/P11.js
+++ W3/D5/P11.js
@@ -2,7 +2,7 @@ const cbtn = document.getElementById("comparebtn");
 cbtn.addEventListener("click",function(e){
     console.log("regular function");
     console.log("this === btn",this===cbtn);
-    console.log("e current target === btn",e.currentTarget===cbtn);
+    console.log("e current  target === btn",e.currentTarget===cbtn);
     console.log("e.target ==btn",e.target ===cbtn);
 });
 
