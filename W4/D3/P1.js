94db46983cf04f1e73f05fbeef6a32ad52fbf339
Raghavendra H S <thenameisragu@gmail.com>
2026-03-15 21:35:33 +0530

    JS:Local and Session Storage View

--- W4/D3/P1.js
+++ W4/D3/P1.js
@@ -2,7 +2,7 @@ const checkbtn =document.getElementById("checkbtn");
 checkbtn.addEventListener("click",function(){
     console.log("Local Storage check",typeof localStorage !=="undefined");
     console.log("localStorage object",localStorage);
-    console.log("Session Storage check",typeof sessionStorage !=="undefined");
+    console.log("Session Storage check",typeof  sessionStorage !=="undefined");
     console.log("Session Storage object",localStorage);
 
 });
