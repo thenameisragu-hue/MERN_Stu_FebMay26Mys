fdd7aa8cc692d3910f0a92ad988878f202355510
Raghavendra H S <thenameisragu@gmail.com>
2026-03-16 19:07:28 +0530

    Main OF Portfolio

---
+++ W4/D5/portfolio-project/js/main.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", 
+    function(){
+        // Render skill
+        renderSkills();
+        initModal();
+        initContactValidation();
+        initThemeToggle();
+        projectSearch();
+    }
+);
