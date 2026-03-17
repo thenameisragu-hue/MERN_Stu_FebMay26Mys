c1d23851536c1293ebe2e38df10bb5dd45ddccc1
Raghavendra H S <thenameisragu@gmail.com>
2026-03-17 14:35:49 +0530

    Callback nesting using name functions

---
+++ W5/D2/P7.js
@@ -0,0 +1,35 @@
+// Callback nesting using name functions
+
+function loadUser(next){
+    setTimeout(function(){
+        console.log("Step 1: User loaded.");
+        next();
+    },400);
+}
+
+function loadOrders(next){
+    setTimeout(function(){
+        console.log("Step 2: Orders loaded");
+        next();
+    },400);
+}
+
+function loadPayments(next){
+    setTimeout(function(){
+        console.log("Step 3: Payments loaded");
+    },400);
+}
+
+function loadShipment(next){
+    setTimeout(function(){
+        console.log("Step 4: Shipment loaded");
+        console.log("Same flow but easier to read");
+    },400);
+}
+loadUser(function(){
+    loadOrders(function(){
+        loadPayments(function(){
+            loadShipment();
+        });
+    });
+});
