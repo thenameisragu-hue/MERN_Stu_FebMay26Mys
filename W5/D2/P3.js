7cb6c9c620f35f446560987a3c4a13b14796ced2
Raghavendra H S <thenameisragu@gmail.com>
2026-03-17 13:30:05 +0530

    Asynchronous approach of Node.js

---
+++ W5/D2/P3.js
@@ -0,0 +1,18 @@
+// Asynchronous approach of Node.js
+
+console.log("step 1: Script started.");
+
+setTimeout(() => {
+
+},1000);
+
+
+setTimeout(function () {
+    console.log("Step 3: Delayed callback finished. F1.");
+},10000);
+
+setTimeout(function () {
+    console.log("Step 3: Delayed callback finished. F2.");
+},3000);
+
+console.log("step 2: Script did not stop while waiting.");
