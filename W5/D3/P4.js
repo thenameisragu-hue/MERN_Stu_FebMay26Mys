1865d310c6f63896caf19b01367036d181bc4688
Raghavendra H S <thenameisragu@gmail.com>
2026-03-18 11:25:11 +0530

    Handle errors with try/catch

--- W5/D3/P4.js
+++ W5/D3/P4.js
@@ -1,1 +1,29 @@
-// combining multiple promise-based steps with async/await
+// Combining multiple promise-based steps with async/await
+
+function getOrderId(){
+    return new Promise(function(resolve){
+        setTimeout(function(){
+            resolve(501);
+        },500);
+    });
+}
+
+function getOrderDetails(orderId){
+    return new Promise(function(resolve){
+        setTimeout(function(resolve){
+            resolve({id:orderId,
+            product:"Laptop",
+            quatity:2
+        });
+        },700);
+    });
+}
+
+async function showOrederSummary(){
+    const orderId = await getOrderId();
+    console.log("Order Id: ",orderId);
+
+    const OrderDetails = await getOrderDetails(orderId);
+    console.log("Product: ",OrderDetails.product);
+    console.log("Quatity: ",OrderDetails.quatity);
+}
